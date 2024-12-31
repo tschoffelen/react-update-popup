68f05442226aa8e6360b6b2c043877afac7f1a41
Thomas Schoffelen <thomas@schof.co>
2024-12-31 11:33:47 +0000

    feat: upgrade for the modern age
    breaking change: changed check logic

--- README.md
+++ README.md
@@ -5,16 +5,12 @@
 [![NPM](https://img.shields.io/npm/v/react-update-popup.svg)](https://www.npmjs.com/package/react-update-popup)
 [![GitHub license](https://img.shields.io/github/license/flexible-agency/react-update-popup.svg)](https://github.com/flexible-agency/react-update-popup/blob/master/LICENSE)
 
-
 ## Install
-
-Works out of the box for create-react-app apps.
 
 ```bash
 yarn add react-update-popup
 # or: npm install --save react-update-popup
 ```
-
 
 ## Usage
 
@@ -25,10 +21,19 @@ import { UpdateNotification } from 'react-update-popup'
 import 'react-update-popup/dist/index.css'
 
 const Example = () => (
-  <UpdateNotification />
+  <UpdateNotification
+    checkHasUpdate={async () => {
+      // Optionally do something custom to check if an update is available.
+      // Called every 120 seconds.
+
+      // If you omit this, the default behaviour is to check if `index.html`
+      // has changed, which is the case with most bundlers.
+
+      return true // tell it an update is available
+    }}
+  />
 )
 ```
-
 
 ## Styling
 
@@ -38,23 +43,22 @@ By default, the popup looks like this:
 
 Update it to fit within your application by:
 
-* Using the component props to change the text of the popup
-* Adding custom CSS for class `.update-notification-popup` to restyle the popup
-
+- Using the component props to change the text of the popup
+- Adding custom CSS for class `.update-notification-popup` to restyle the popup
 
 ## Props
 
 All props are optional, and can be used to change the appearance of the popup.
 
-* string `title` - popup title, wrapped in `<h3>`
-* string `description` - popup description text, wrapped in `<p>`
-* string `buttonText` - button text
-* string `assetManifestUrl` - this is how we check if the React app has been updated, defaults to `/asset-manifest.json`
-* function `onReload` - replace default reload behaviour
-
+- string `title` - popup title, wrapped in `<h3>`
+- string `description` - popup description text, wrapped in `<p>`
+- string `buttonText` - button text
+- number `refreshInterval` - how often to check, defaults to `120_000` (2 minutes)
+- function `checkHasUpdate` - replace default update check behaviour (returns boolean `hasUpdate`)
+- function `onReload` - replace default reload behaviour
 
 ## Authors
 
-This library is developed by Flexible Agency, a creative app development agency.
+This library is developed by [Includable](https://includable.com), a creative app development agency.
 
 - Thomas Schoffelen, [@tschoffelen](https://twitter.com/tschoffelen)
--- src/index.tsx
+++ src/index.tsx
@@ -1,58 +1,95 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react'
 import useSWR from 'swr'
-
-import { ReactComponent as Icon } from './icon.svg'
-import './styles.css'
 
 interface Props {
   title?: string
   description?: string
   buttonText?: string
-  assetManifestUrl?: string
+  checkHasUpdate?: () => Promise<boolean>
+  refreshInterval?: number
   onReload?: () => void
 }
 
-const assetManifestFetcher = (assetManifestUrl: string) =>
-  axios.get(`${assetManifestUrl}?_${new Date().getTime()}`, {
-    responseType: 'text',
-    transformResponse: [(data) => data]
+let previousResponse = ''
+export const defaultUpdateChecker: () => Promise<boolean> = async () => {
+  const response = await fetch(`/index.html?_${new Date().getTime()}`, {})
+  const text = await response.text()
+
+  if (!previousResponse) {
+    previousResponse = text
+  } else if (text !== previousResponse) {
+    return true
+  }
+
+  return false
+}
+
+export const UpdateNotification = ({
+  title = 'App update available',
+  description = 'Reload this page to use the latest version of the app.',
+  buttonText = 'Reload now',
+  checkHasUpdate = defaultUpdateChecker,
+  refreshInterval = 120000,
+  onReload = () => typeof window !== 'undefined' && window.location.reload()
+}: Props) => {
+  const { data: updateAvailable } = useSWR('', checkHasUpdate, {
+    refreshInterval
   })
 
-// noinspection JSUnusedGlobalSymbols
-export const UpdateNotification = (
-  {
-    title = 'App update available',
-    description = 'Reload this page to use the latest version of the app.',
-    buttonText = 'Reload now',
-    assetManifestUrl = '/asset-manifest.json',
-    onReload
-  }: Props) => {
-  const [assetManifest, setAssetManifest] = useState('')
-  const { data } = useSWR(assetManifestUrl, assetManifestFetcher, {
-    refreshInterval: 120000
-  })
-  useEffect(() => {
-    if (!assetManifest && data) {
-      setAssetManifest(data.data)
-    }
-  }, [data, assetManifest])
-
-  onReload = onReload || (() => typeof window !== 'undefined' && window.location.reload())
-
-  const updateAvailable = assetManifest && data && data.data !== assetManifest
   if (!updateAvailable) {
     return null
   }
 
   return (
     <div className='update-notification-popup'>
-      <Icon />
-      <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <button onClick={onReload}>{buttonText}</button>
-      </div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <button onClick={onReload}>{buttonText}</button>
+      <style>
+        {`
+          .update-notification-popup {
+            position: fixed;
+            bottom: 24px;
+            right: 24px;
+            box-shadow: 0 -1px 6px rgba(0, 0, 0, 0.02), 0 1px 4px rgba(0, 0, 0, 0.05),
+              0 10px 30px rgba(0, 0, 0, 0.02);
+            border-radius: 6px;
+            background-color: #fff;
+            padding: 24px;
+            max-width: calc(100vw - 48px);
+            width: 300px;
+          }
+
+          .update-notification-popup * {
+            margin: 0;
+            padding: 0;
+          }
+
+          .update-notification-popup h3 {
+            font-size: 1.1rem;
+            font-weight: 600;
+            line-height: 1.1rem;
+            margin-bottom: 10px;
+          }
+
+          .update-notification-popup p {
+            font-size: 0.9rem;
+          }
+
+          .update-notification-popup button {
+            background-color: #2977f6;
+            border-radius: 4px;
+            color: #fff;
+            border: 0;
+            padding: 0 16px;
+            font-size: 0.9rem;
+            height: 38px;
+            font-weight: 600;
+            margin-top: 22px;
+            display: inline-block;
+          }
+        `}
+      </style>
     </div>
   )
 }
